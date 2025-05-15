a56e62dfa1ffb9fbc6c9cb76dc18c61757bcd9aa
Raunak Dubey <rounakdubey2009@gmail.com>
2025-05-15 21:06:05 +0530

    Initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+app.get("/demouser",async (req,res)=>{
+//     let fakeUser=new User({
+//         email:"abc123@getMaxListeners.com",
+//         username:"new-student",
+//     });
+//     let newUser=await User.register(fakeUser,'12345678');
+//     res.send(newUser);
+
+// })
